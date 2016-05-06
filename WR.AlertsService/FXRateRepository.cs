4fb9e463d100377562f1ec7c1f16afbe04025ca5
Carlos Ruiz <cruiz@worldremit.com>
2016-05-06 10:57:03 +0100

    Integration with Notification and Repository

---
+++ WR.AlertsService/FXRateRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using WR.AlertsService;
+
+namespace NotificationService
+{
+    public interface IFxRateRepository
+    {
+        IEnumerable<Rate> GetRates(string fromCurrency, string toCurrency, DateTime timestamp);
+    }
+
+    public class FxRateRepository : IFxRateRepository
+    {
+        public IEnumerable<Rate> GetRates(string fromCurrency, string toCurrency, DateTime timestamp)
+        {
+            var fxRates = new List<Rate>();
+            var lines = File.ReadAllLines("TestFxRates.txt");
+            foreach (var line in lines)
+            {
+                var splittedLines = line.Split(',');
+                var rate = CreateRate(splittedLines);
+                fxRates.Add(rate);
+            }
+            return fxRates;
+        }
+
+        private static Rate CreateRate(string[] splittedLines)
+        {
+            return new Rate()
+            {
+                FromCurrency = splittedLines[0],
+                ToCurrency = splittedLines[1],
+                ExchangeRate = decimal.Parse(splittedLines[3]),
+                ValidFrom = DateTime.Parse(splittedLines[4]),
+                ValidTo = DateTime.Parse(splittedLines[4]).AddMinutes(1)
+            };
+        }
+    }
+}
---
+++ WR.AlertsService/SMSNotification.cs
@@ -0,0 +1,18 @@
+﻿using Twilio;
+
+namespace NotificationService
+{
+    public class SMSNotification
+    {
+        private  string AccountSid = "AC86ea011ed3fe1293647e41bc8910a9d7";
+        private  string AuthToken = "05a3af8210b71fe7e2cef7ca17e39200";
+        private readonly string fromNumber = "+441286727032";
+
+        public void SendSMS(string toNumber, string message)
+        {
+            var twilio = new TwilioRestClient(AccountSid, AuthToken);
+
+            var ms = twilio.SendMessage(fromNumber, toNumber, message);
+        }
+    }
+}
